f03e35ac5fa7eb8ff0af81476a92de0343ac0d4d
Anitha <71427001+anianitha835@users.noreply.github.com>
2021-06-29 18:55:26 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=1;i<=10;i++)
+    {
+        System.out.println(n +" x " + i +" = " +n*i);
+    }
+  }
+}
+
+
+
+
+output:
+2
+
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+    
